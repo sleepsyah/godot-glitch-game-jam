a5a99022bf9ef423b91b469d92c6ac22b6eb077d
syah <xzhou0306@gmail.com>
2025-08-18 20:24:40 -0600

    mini "jumpscare" added

--- glitch-@ttract/scenes/clown.gd
+++ glitch-@ttract/scenes/clown.gd
@@ -1,13 +1,11 @@
-extends Button
+extends Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if button_pressed:
-		get_tree().change_scene_to_file("res://scenes/comment_screen.tscn")
-		Global.good_post_count +=1
+	pass
