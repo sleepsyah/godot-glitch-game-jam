5ec6ba9b0d1cdc5ff5df6f6002a7e77cb2c9b6b0
syah <xzhou0306@gmail.com>
2025-08-19 19:38:38 -0600

    i think i fixed it

--- glitch-@ttract/scenes/post_1.gd
+++ glitch-@ttract/scenes/post_1.gd
@@ -1,10 +1,11 @@
 extends Button
 
-var clown = ResourceLoader.load("res://clownscary.png")
-@onready var sound = $AudioStreamPlayer
+#var clown = ResourceLoader.load("res://clownscary.png")
+#@onready var sound = $AudioStreamPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"../clown".scale = Vector2(0,0)
+	pass
+#	$"../clown".scale = Vector2(0,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +18,9 @@ func _on_pressed() -> void:
 	#Global.bad_post_count+=1
 		#do jumpscare
 	
-	var tween = create_tween()
-	tween.tween_property($"../clown",'scale',Vector2(5,5),1)
-	sound.play()
+	#var tween = create_tween()
+	#tween.tween_property($"../clown",'scale',Vector2(5,5),1)
+	#sound.play()
 	#	clown.show()$"../clown"
-	await get_tree().create_timer(1.0).timeout
+	#await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://scenes/comment_screen.tscn")
