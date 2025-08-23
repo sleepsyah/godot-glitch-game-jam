70edebcada0e67b72708093f2314aaf5d8c809a0
syah <xzhou0306@gmail.com>
2025-08-23 14:17:22 -0600

    msg animation finished

--- glitch-@ttract/scenes/messages_screen_2.gd
+++ glitch-@ttract/scenes/messages_screen_2.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
 func _ready() -> void:
-	pass
-	
+	$goending.hide()
+	await get_tree().create_timer(1.0).timeout
+	$"Andrea-de-santis-3yqmF5iy5c-unsplash".position = Vector2(592,312)
+	await get_tree().create_timer(1.5).timeout
+	$interface/me1.position=Vector2(416,400)
+	await get_tree().create_timer(1.5).timeout
+	$interface/unknown2.position=Vector2(416,448)
+	await get_tree().create_timer(1.5).timeout
+	$interface/me2.position=Vector2(416,496)
+	await get_tree().create_timer(1.5).timeout
+	$goending.show()
 func _on_goending_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/hackScreen.tscn")
--- glitch-@ttract/scenes/scene_1.gd
+++ glitch-@ttract/scenes/scene_1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready() -> void:
 	$Button.hide()
-	await get_tree().create_timer(1.5).timeout
+	await get_tree().create_timer(1.0).timeout
 	$interface/me1.position = Vector2(416,192)
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown2.position = Vector2(416,248)
