baadd97cf4ab62bb4d291c9f981d5f1996832ee7
syah <xzhou0306@gmail.com>
2025-08-23 13:21:44 -0600

    msgs

--- glitch-@ttract/scenes/scene_1.gd
+++ glitch-@ttract/scenes/scene_1.gd
@@ -1,1 +1,20 @@
 extends Node2D
+
+func _ready() -> void:
+	$Button.hide()
+	await get_tree().create_timer(1.5).timeout
+	$interface/me1.position = Vector2(416,192)
+	await get_tree().create_timer(1.5).timeout
+	$interface/unknown2.position = Vector2(416,248)
+	await get_tree().create_timer(1.5).timeout
+	$interface/unknown3.position = Vector2(528,288)
+	await get_tree().create_timer(1.5).timeout
+	$interface/unknown4.position = Vector2(528,328)
+	await get_tree().create_timer(1.5).timeout
+	$interface/unknown5.position = Vector2(528,368)
+	await get_tree().create_timer(1.5).timeout
+	$interface/me2.position = Vector2(416,424)
+	await get_tree().create_timer(1.5).timeout
+	$interface/unknown6.position = Vector2(416,480)
+	await get_tree().create_timer(1.5).timeout
+	$Button.show()
