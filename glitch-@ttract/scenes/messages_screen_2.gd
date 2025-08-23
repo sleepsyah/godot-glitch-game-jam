43da38f88e82a46fd226b435e3e8debbec60605f
syah <xzhou0306@gmail.com>
2025-08-23 16:55:43 -0600

    added notifications again

--- glitch-@ttract/scenes/messages_screen_2.gd
+++ glitch-@ttract/scenes/messages_screen_2.gd
@@ -4,12 +4,16 @@ func _ready() -> void:
 	$goending.hide()
 	await get_tree().create_timer(1.0).timeout
 	$"Andrea-de-santis-3yqmF5iy5c-unsplash".position = Vector2(592,312)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/me1.position=Vector2(416,400)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown2.position=Vector2(416,448)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/me2.position=Vector2(416,496)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$goending.show()
 func _on_goending_pressed() -> void:
--- glitch-@ttract/scenes/scene_1.gd
+++ glitch-@ttract/scenes/scene_1.gd
@@ -4,17 +4,24 @@ func _ready() -> void:
 	$Button.hide()
 	await get_tree().create_timer(1.0).timeout
 	$interface/me1.position = Vector2(416,192)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown2.position = Vector2(416,248)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown3.position = Vector2(528,288)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown4.position = Vector2(528,328)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown5.position = Vector2(528,368)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/me2.position = Vector2(416,424)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$interface/unknown6.position = Vector2(416,480)
+	$notification.play()
 	await get_tree().create_timer(1.5).timeout
 	$Button.show()
